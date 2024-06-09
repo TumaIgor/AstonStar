57260a4cefcc9b7b6af775512301903f86b28cbd
TumaIgor <tuea@rambler.ru>
2024-06-09 18:22:00 +0300

    Aston_HW_2

---
+++ Aston_HW_2/src/aston/list/test/ArrayListTest.java
@@ -0,0 +1,41 @@
+package aston.list.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import aston.list.interfaces.RealizeList;
+import aston.list.models.MyArrayList;
+
+class ArrayListTest {
+
+
+	@Test
+	void test() {
+		RealizeList<Integer> list = new MyArrayList<>();
+		System.out.println(list.size());
+		list.add(2);
+		list.add(7);
+		list.add(3);
+		list.add(5);
+		list.add(null);
+		list.add(7);
+		System.out.println(list.size());
+		System.out.println(list.get(4));
+		int index = list.indexOf(null);
+		System.out.println(index);
+		index = list.indexOf(5);
+		System.out.println(index);
+		System.out.println(list.contains(12));
+		System.out.println("==================");
+		System.out.println(list.size());
+		int num = list.remove(3);
+		System.out.println(list.size());
+		System.out.println(num);
+		System.out.println("==================");
+		
+	
+		
+	}
+
+}
